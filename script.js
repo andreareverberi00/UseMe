dfe1b29be6320413ab7900d4ce00816a9c38e886
Edoardo Gargano <edoardo.gargano@paramount.com>
2023-09-01 09:49:58 +0200

    fix Mpd and prettier

--- script.js
+++ script.js
@@ -1,41 +1,38 @@
-const assetURL = 'https://nickdesaulniers.github.io/netfix/demo/frag_bunny.mp4'
+const assetURL = "https://nickdesaulniers.github.io/netfix/demo/frag_bunny.mp4";
 
-var video = document.getElementById('mse-video');
+var video = document.getElementById("mse-video");
 
 // Need to be specific for Blink regarding codecs
 // ./mp4info frag_bunny.mp4 | grep Codec
 var mimeCodec = 'video/mp4; codecs="avc1.42E01E, mp4a.40.2"';
 
-if ('MediaSource' in window && MediaSource.isTypeSupported(mimeCodec)) {
-    var mediaSource = new MediaSource;
-    //console.log(mediaSource.readyState); // closed
-    video.src = URL.createObjectURL(mediaSource);
-    mediaSource.addEventListener('sourceopen', sourceOpen);
+if ("MediaSource" in window && MediaSource.isTypeSupported(mimeCodec)) {
+  var mediaSource = new MediaSource();
+  video.src = URL.createObjectURL(mediaSource);
+  mediaSource.addEventListener("sourceopen", sourceOpen);
 } else {
-    console.error('Unsupported MIME type or codec: ', mimeCodec);
+  console.error("Unsupported MIME type or codec: ", mimeCodec);
 }
 
 function sourceOpen(_) {
-    //console.log(this.readyState); // open
-    var mediaSource = this;
-    var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
-    fetchAB(assetURL, function (buf) {
-        sourceBuffer.addEventListener('updateend', function (_) {
-            mediaSource.endOfStream();
-            video.play();
-            //console.log(mediaSource.readyState); // ended
-        });
-        sourceBuffer.appendBuffer(buf);
+  var mediaSource = this;
+  var sourceBuffer = mediaSource.addSourceBuffer(mimeCodec);
+  fetchAB(assetURL, function (buf) {
+    sourceBuffer.addEventListener("updateend", function (_) {
+      mediaSource.endOfStream();
+      video.play();
     });
-};
+    sourceBuffer.appendBuffer(buf);
+  });
+}
 
 function fetchAB(url, cb) {
-    console.log(url);
-    var xhr = new XMLHttpRequest;
-    xhr.open('get', url);
-    xhr.responseType = 'arraybuffer';
-    xhr.onload = function () {
-        cb(xhr.response);
-    };
-    xhr.send();
-};
+  console.log(url);
+  var xhr = new XMLHttpRequest();
+  xhr.open("get", url);
+  xhr.responseType = "arraybuffer";
+  xhr.onload = function () {
+    cb(xhr.response);
+  };
+  xhr.send();
+}
